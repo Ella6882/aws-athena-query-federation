1b5bcd815bcd146c17a033348c8a58fc75fbc1f4
Ella Wherritt <ella.wherritt@agilebits.com>
2022-12-16 11:22:21 -0700

    user agent parse added

---
+++ my-athena-udfs/src/main/java/o-kz3v3bzqqf/App.java
@@ -0,0 +1,13 @@
+package o-kz3v3bzqqf;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
